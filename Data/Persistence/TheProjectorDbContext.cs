60a444233b98e1515b69b9da245231e5fb5c55e8
Erard Valdez <valdez.erardtimothy@gmail.com>
2023-03-21 21:28:39 +0800

    remove intermediate entity (clears data of assigned people on projects)

--- Data/Persistence/TheProjectorDbContext.cs
+++ Data/Persistence/TheProjectorDbContext.cs
@@ -9,8 +9,6 @@ public class TheProjectorDbContext : DbContext
 
     public DbSet<Person> People { get; set; }
 
-    public DbSet<PersonProjectAssignment> PersonProjectAssignments { get; set; }
-
     public TheProjectorDbContext(DbContextOptions<TheProjectorDbContext> options)
     : base(options)
     {
@@ -18,16 +16,5 @@ public class TheProjectorDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Project>()
-        .HasMany(project => project.AssignedPeople)
-        .WithMany(person => person.AssignedProjects)
-        .UsingEntity<PersonProjectAssignment>(
-            j => j.HasOne(ppa => ppa.Person)
-                .WithMany()
-                .OnDelete(DeleteBehavior.Restrict),
-            j => j.HasOne(ppa => ppa.Project)
-                .WithMany()
-                .OnDelete(DeleteBehavior.Restrict)
-        );
     }
 }
---
+++ Migrations/20230321132244_RemoveIntermediateProjectPersonEntity.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheProjector.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIntermediateProjectPersonEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonProjectAssignments");
+
+            migrationBuilder.CreateTable(
+                name: "PersonProject",
+                columns: table => new
+                {
+                    AssignedPeopleId = table.Column<long>(type: "bigint", nullable: false),
+                    AssignedProjectsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonProject", x => new { x.AssignedPeopleId, x.AssignedProjectsId });
+                    table.ForeignKey(
+                        name: "FK_PersonProject_People_AssignedPeopleId",
+                        column: x => x.AssignedPeopleId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonProject_Projects_AssignedProjectsId",
+                        column: x => x.AssignedProjectsId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonProject_AssignedProjectsId",
+                table: "PersonProject",
+                column: "AssignedProjectsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonProject");
+
+            migrationBuilder.CreateTable(
+                name: "PersonProjectAssignments",
+                columns: table => new
+                {
+                    PersonId = table.Column<long>(type: "bigint", nullable: false),
+                    ProjectId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonProjectAssignments", x => new { x.PersonId, x.ProjectId });
+                    table.ForeignKey(
+                        name: "FK_PersonProjectAssignments_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PersonProjectAssignments_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonProjectAssignments_ProjectId",
+                table: "PersonProjectAssignments",
+                column: "ProjectId");
+        }
+    }
+}
